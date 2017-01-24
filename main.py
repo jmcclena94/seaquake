0cebda1dc06355ae4111440cd04eeb8a6a6001d3
Joseph McClenahan <jmcclena94@gmail.com>
2017-01-23 22:09:21 -0800

    updated quake data, removed html folder

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from sanic import Sanic
 # from sanic.response import text
-from sanic.response import json
+# from sanic.response import json
+from sanic.response import html
 from get_quake_data import get_quake_data
+from jinja2 import Environment, PackageLoader
+
+env = Environment(
+    loader=PackageLoader('main', './static/html'),
+)
 
 app = Sanic(__name__)
 
 @app.route("/")
 async def test(request):
-    # app.static('/', './static/html/landing.html')
-    return json(get_quake_data())
+    template = env.get_template('landing.html')
+    data = get_quake_data()
+    # import pdb; pdb.set_trace()
+    return html(template.render(quakes=data))
 
 
 app.run(host="0.0.0.0", port=8000, debug=True)
