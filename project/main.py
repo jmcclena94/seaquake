3c5dd9b26bacd465b0c55fca2f1c1cfe9eec3e60
Joseph McClenahan <jmcclena94@gmail.com>
2017-01-24 14:06:55 -0800

    updated project structure

--- project/main.py
+++ project/main.py
@@ -2,14 +2,15 @@ from sanic import Sanic
 from sanic.response import json, html
 import os
 from get_quake_data import get_quake_data
+import settings
 
 app = Sanic(__name__)
-app.static('/static', './static')
+app.static('/static', settings.STATIC_DIR)
+
 
 @app.route("/")
 async def test(request):
-    template = open(os.getcwd() + "/index.html")
-    print(os.getcwd())
+    template = open(os.getcwd() + "/project/index.html")
     return html(template.read())
 
 @app.route("/json")
